c0b8b120a5fa3d244aea9ca4f52c8ead30d29c54
welfric <ahmadashraf00001@gmail.com>
2025-09-19 23:20:14 +0500

    max temp = 1.0

--- models.py
+++ models.py
@@ -69,7 +69,7 @@ class ShuffLeNet(nn.Module):
         p = self.classifier(z)
         return z,p
 class TempNet(nn.Module):
-    def __init__(self, feature_dim=512, hidden_dim=128, tau_min=0.1, tau_max=2.0):
+    def __init__(self, feature_dim=512, hidden_dim=128, tau_min=0.1, tau_max=1.0):
         super().__init__()
         self.fc1 = nn.Linear(feature_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, 1)
