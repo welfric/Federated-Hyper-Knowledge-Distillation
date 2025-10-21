3ed555b0e902edc2ac99fc909b6597f5fc69e299
welfric <ahmadashraf00001@gmail.com>
2025-10-22 02:19:55 +0500

    sgd HKD

--- Client/ClientFedHKD.py
+++ Client/ClientFedHKD.py
@@ -29,7 +29,7 @@ class ClientFedHKD(Client):
         self.model.to(self.device)
         self.model.train()
         epoch_loss = []
-        optimizer = optim.Adam(self.model.parameters(),lr=self.args.lr)
+        optimizer = optim.SGD(self.model.parameters(),lr=self.args.lr)
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=self.args.lr_sh_rate, gamma=0.5)
         for iter in range(self.args.local_ep):
             batch_loss = []
