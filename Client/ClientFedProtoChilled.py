4b3af5cc2173599bb5318a859ab11cb6d267b6bf
welfric <ahmadashraf00001@gmail.com>
2025-11-04 02:32:21 +0500

    xd2

--- Client/ClientFedProtoChilled.py
+++ Client/ClientFedProtoChilled.py
@@ -105,7 +105,6 @@ class ClientFedProto(Client):
                         global_round, iter, batch_idx * len(X),
                         len(self.trainloader.dataset),
                         100. * batch_idx / len(self.trainloader), loss1.item(),loss2.item()))
-                self.logger.add_scalar('loss', loss.item())
                 batch_loss.append(loss.item())
             epoch_loss.append(sum(batch_loss)/len(batch_loss))
                         
